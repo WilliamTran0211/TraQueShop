561b06a7d07696994a20fbb8d0411d8013ed657e
thiennam0211 <tiencuongtran.0211@gmail.com>
2018-12-28 13:33:05 +0700

    Create

---
+++ TraQueShop/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TraQueShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult TrangChu()
+        {
+            var danhmuc = new List<string>() {
+                "Hoa Hong", "Hoa Cuoi", "Hoa Khai Truong"
+            };
+            ViewBag.Test = danhmuc;
+            return View();
+        }
+        public ActionResult GioiThieu()
+        {
+            return View();
+        }
+        public ActionResult TinTuc()
+        {
+            return View();
+        }
+    }
+}
---
+++ TraQueShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TraQueShop.MvcApplication" Language="C#" %>
